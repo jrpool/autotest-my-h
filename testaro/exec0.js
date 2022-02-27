dc41918a3e8d3c2c237280ae089f74418c67efa8
Jonathan Pool <pool@stulta.com>
2022-02-27 11:22:03 -0500

    Added resources for Testaro

---
+++ testaro/exec0.js
@@ -0,0 +1,9 @@
+// exec0.js
+// Executor 0 for Testaro
+
+const options = {
+  reports: '../../reports/testaro',
+  script: '../../scripts/testaro.json'
+};
+const {handleRequest} = require('../../../testaro/index');
+handleRequest(options);
